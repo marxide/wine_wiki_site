591b3266e39f57053b7db6640860167fc17b1f7a
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-08-18 00:12:25 +1000

    adds variety to admin page

--- wine_wiki/admin.py
+++ wine_wiki/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Wine, Producer
+from .models import Wine, Producer, Variety
 
 admin.site.register(Wine)
 admin.site.register(Producer)
+admin.site.register(Variety)

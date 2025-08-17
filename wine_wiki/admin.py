45069da74deb781f5b0913c06dde2cee6f51522f
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-08-18 00:32:53 +1000

    adds search fields and filters through model admin classes for wine, producer and variety

--- wine_wiki/admin.py
+++ wine_wiki/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Wine, Producer, Variety
 
-admin.site.register(Wine)
-admin.site.register(Producer)
-admin.site.register(Variety)
+
+class WineAdmin(admin.ModelAdmin):
+    list_filter = ("producer__name", "variety__name")
+    search_fields = ("producer__name", "variety__name")
+
+
+class ProducerAdmin(admin.ModelAdmin):
+    list_filter = ("name", "region")
+    search_fields = ("name", "region")
+
+
+class VarietyAdmin(admin.ModelAdmin):
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Producer, ProducerAdmin)
+admin.site.register(Variety, VarietyAdmin)

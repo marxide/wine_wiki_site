24bab1a037ecf8f73c21cde7e433bf9f509542cb
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-08-19 12:05:43 +1000

    adds general dev db dir and path

--- wine_wiki_site/dev_settings.py
+++ wine_wiki_site/dev_settings.py
@@ -3,3 +3,11 @@ from .settings import *  # noqa
 ALLOWED_HOSTS.append("127.0.0.1")
 
 DEBUG = True
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "dev_db" / "db.sqlite3",
+    }
+}
